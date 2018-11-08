b986b12c4d7bc181c3473486e385e21ada68a4c6
aboud20-meet <aboud20@meet.mit.edu>
2018-11-08 19:36:16 +0200

    33

--- database.py
+++ database.py
@@ -8,11 +8,19 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def create_cat(name):
-    cat_object = Cat(name=name)
+def create_cat(name ,image):
+    cat_object = Cat(name=name , image=image)
     session.add(cat_object)
     session.commit()
+
+cat_jo = create_cat (name="jo" , image="cat.jpg")
+
 
 def get_all_cats():
     cats = session.query(Cat).all()
     return cats
+
+def get_cat(id):
+
+	cat = session.query(Cat).filter_by(id=id).first()
+	return cat
